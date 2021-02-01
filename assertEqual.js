5b5980a9680819be5fbfd2d3f48f0607664b4ebb
JoshuaRully <joshrully@gmail.com>
2021-02-01 23:18:00 +0000

    checks if inputs are equal then yields corresponding output

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// Function implementation
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected + "");
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected + "");
+  }
+};
+
+// tests below
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("coffee", "tea");
+assertEqual("crimp", "sloper");
+assertEqual(456, 456);
+assertEqual("dog", "dog");
