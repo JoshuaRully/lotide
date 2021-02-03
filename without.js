dc65b055d921303dddd5a8f4050b0878a2489b1b
JoshuaRully <joshrully@gmail.com>
2021-02-03 03:59:41 +0000

    checked if original was mutated using test case

--- without.js
+++ without.js
@@ -26,3 +26,7 @@ const without = (arr, filterArr) => {
 
 // tests below
 assertArraysEqual(without([1, 2, 3, 4, 5, 3, 1, 4], [3, 1]), [2, 4, 5, 4]);
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
