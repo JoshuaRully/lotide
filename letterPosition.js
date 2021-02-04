8b96be487b86a00938f6c61ea5670e0f0f915efc
Joshua Rully <joshrully@gmail.com>
2021-02-04 02:45:18 +0000

    function that returns an object containing a count of all occurring letters in a string

---
+++ letterPosition.js
@@ -0,0 +1,31 @@
+const eqArrays = (arrayOne, arrayTwo) => {
+  return Array.isArray(arrayOne) && Array.isArray(arrayTwo) && arrayOne.length === arrayTwo.length && arrayOne.every((val, index) => val === arrayTwo[index]);
+};
+
+const assertArraysEqual = (arrayOne, arrayTwo) => {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+const letterPositions = (string) => {
+  const results = {};
+  string = string.toLowerCase().split("");
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === " ") {
+      return results;
+    } else if (results[string[i]]) {
+      results[string[i]].push(i);
+    } else {
+      results[string[i]] = [i];
+    }
+  }
+  return results;
+};
+
+// tests below
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("coffee").f, [2, 3]);
