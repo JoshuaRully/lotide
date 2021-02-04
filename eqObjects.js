3b99276bbb89f2e7ba6d75b3f389abc9957aad91
Joshua Rully <joshrully@gmail.com>
2021-02-04 03:11:10 +0000

    function that takes two objects and returns true or false if strict match

---
+++ eqObjects.js
@@ -0,0 +1,46 @@
+// functions implemented
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arrayOne, arrayTwo) => {
+  return Array.isArray(arrayOne) && Array.isArray(arrayTwo) && arrayOne.length === arrayTwo.length && arrayOne.every((val, index) => val === arrayTwo[index]);
+};
+
+// function below
+const eqObjects = (obj1, obj2) => {
+  let arr1 = Object.keys(obj1);
+  let arr2 = Object.keys(obj2);
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let key in obj1) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) return false;
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// tests below
+const ab = { a: "1", b: "2"};
+const ba = { b: "2", a: "1"};
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = {a: "1", b: "2", c: "3"};
+assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3] };
+assertEqual(eqObjects(cd, cd2), true); // => true
