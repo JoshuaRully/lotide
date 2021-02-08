e3af78c850aa907da2beb76394a9a45c207ad12c
Joshua Rully <joshrully@gmail.com>
2021-02-08 17:09:15 +0000

    refactored assertEqual to only contain the function and moved its test code to test/

--- assertEqual.js
+++ assertEqual.js
@@ -8,11 +8,3 @@ const assertEqual = (actual, expected) => {
 };
 
 module.exports = assertEqual;
-
-// tests below
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual("coffee", "tea");
-// assertEqual("crimp", "sloper");
-// assertEqual(456, 456);
-// assertEqual("dog", "dog");
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// tests below
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("coffee", "tea");
+assertEqual("crimp", "sloper");
+assertEqual(456, 456);
+assertEqual("dog", "dog");
