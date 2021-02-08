f48548d94d3f033de625528c9f723d8e52865d24
Joshua Rully <joshrully@gmail.com>
2021-02-08 20:53:31 +0000

    refactored middle.test to use mocha chai test cases

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,7 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// test cases below
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
-assertArraysEqual(middle([]), []);
+describe("middle function tests", () => {
+  it("should return [3, 4] from [1, 2, 3, 4, 5, 6]", () => {
+    assert(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("should return [4] from [1, 2, 3, 4, 5, 6, 7]", () => {
+    assert(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+  });
+
+  it("should return [] from []", () => {
+    assert(middle([]), []);
+  });
+});
