935bd9af97e2b12b926c34dae0ef5f730988fafd
Joshua Rully <joshrully@gmail.com>
2021-02-08 20:55:40 +0000

    minor but important change of strictEqual to deepEqual to middle.test

--- test/middle.test.js
+++ test/middle.test.js
@@ -3,14 +3,14 @@ const middle = require('../middle');
 
 describe("middle function tests", () => {
   it("should return [3, 4] from [1, 2, 3, 4, 5, 6]", () => {
-    assert(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 
   it("should return [4] from [1, 2, 3, 4, 5, 6, 7]", () => {
-    assert(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
   });
 
   it("should return [] from []", () => {
-    assert(middle([]), []);
+    assert.deepEqual(middle([]), []);
   });
 });
